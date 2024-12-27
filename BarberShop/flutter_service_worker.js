ea8ee9ca1de4fe8211ff18296e32535ea2127581
mrisatmo <46067963+mrisatmo@users.noreply.github.com>
2024-12-27 22:50:35 +0200

    add the update version of barbershiop
    add the barbershop updated UI

--- BarberShop/flutter_service_worker.js
+++ BarberShop/flutter_service_worker.js
@@ -23,14 +23,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "49dabd07ae54210cd04bc86222a13624",
+"flutter_bootstrap.js": "f40f8415bce83fa346bff27feeb7a580",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "14fbe91ab2fc38bde1551c20c3c9df05",
 "/": "14fbe91ab2fc38bde1551c20c3c9df05",
-"main.dart.js": "398fa4707da9c3fa0a6de47ee4ff95a6",
+"main.dart.js": "dcf2465d1dca5196238be19d89c9f842",
 "manifest.json": "07998826188c61ea625c45162cdb788e",
 "version.json": "6a0280a191188b711cda310f925e9bd3"};
 // The application shell files that are downloaded before a service worker can
